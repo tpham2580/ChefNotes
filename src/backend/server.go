98f095a6f1ca18275271749684893f9ac345fc66
tpham2580 <tpham2580@gmail.com>
2021-08-01 20:50:08 -0500

    added mux package and set up test server

---
+++ src/backend/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	//"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	//_ "github.com/lib/pq"
+)
+
+func main() {
+	router := mux.NewRouter()
+	const port string = ":8000"
+	router.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		fmt.Println(response, "Up and running...")
+	})
+	log.Println("Server listening on port", port)
+	log.Fatalln(http.ListenAndServe(port, router))
+}
