5c5e33ecc4a0ab963030cb1e05e3381d5c8ec78d
tpham2580 <tpham2580@gmail.com>
2021-08-05 20:40:29 -0500

    added sample get request

--- src/backend/route.go
+++ src/backend/route.go
@@ -1,7 +1,9 @@
 package main
 
-//"encoding/json"
-//"net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Post struct {
 	Id    int    `json:"id"`
@@ -17,6 +19,14 @@ func init() {
 	posts = []Post{{Id: 1, Title: "Title 1", Text: "Text 1"}}
 }
 
-func getPosts() {
-
+func getPosts(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	result, err := json.Marshal(posts)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error marshalling the posts array"}`))
+		return
+	}
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(result)
 }
--- src/backend/server.go
+++ src/backend/server.go
@@ -16,6 +16,7 @@ func main() {
 	router.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintln(response, "Up and running...")
 	})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
 	log.Println("Server listening on port", port)
 	log.Fatalln(http.ListenAndServe(port, router))
 }
